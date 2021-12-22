2e8f1cfb9994ba3927958e919762232942e02215
Pedro Augusto Pereira Florentino <69980288+Pedro749@users.noreply.github.com>
2021-12-22 10:39:01 -0300

    Update scroll-animation.js

--- js/modules/scroll-animation.js
+++ js/modules/scroll-animation.js
@@ -3,7 +3,7 @@ import debounce from './debounce.js';
 export default class AnimacaoScroll {
   constructor(sections) {
     this.sections = document.querySelectorAll(sections);
-    this.windowMetade = window.innerHeight * 0.6;
+    this.windowMetade = window.innerHeight * 0.4;
     this.checkDistance = debounce(this.checkDistance.bind(this), 50);
   }
 
